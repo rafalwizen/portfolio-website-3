82a190e7cda07abb8ee012ea677a41b3b8aaf83c
Rafal Wizen <rafal.wizen@gmail.com>
2025-10-11 15:08:28 +0200

    Refactor: Remove Toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -59,7 +58,6 @@ export default function RootLayout({
         <html lang="pl" className="scroll-smooth">
         <body className={inter.className}>
         {children}
-        <Toaster />
         </body>
         </html>
     )
